44e062ef1e36c232071d680e19c4f67ee1f4ac28
Jaya Kuncara <jaya.ismiku@gmail.com>
2023-10-17 15:51:54 +0700

    first commit
    commit first

---
+++ open-music-submission/src/services/redis/CacheService.js
@@ -0,0 +1,40 @@
+const redis = require('redis');
+const { config } = require('../../commons/config');
+
+class CacheService {
+  constructor() {
+    this._client = redis.createClient({
+      host: config.redis.url,
+    });
+
+    this._client.on('error', (error) => {
+      console.error(error);
+    });
+
+    this._client.connect();
+  }
+
+  async set(key, value, expirationInSecond = 1800) {
+    await this._client.set(key, value, {
+      EX: expirationInSecond,
+    });
+  }
+
+  async get(key) {
+    const result = await this._client.get(key);
+
+    if (!result) {
+      throw new Error('Cache tidak ditemukan');
+    }
+
+    return result;
+  }
+
+  async delete(key) {
+    // TODO-9: Panggil method `del` dari `this._client` untuk menghapus key dari cache.
+    // Referensi: https://www.dicoding.com/academies/271/tutorials/17819
+    this._client.del(key);
+  }
+}
+
+module.exports = CacheService;
